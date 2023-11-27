94f738adf7fd2d8e4befa61a51ccb38a66235aef
I <>
2023-11-27 00:12:10 +0000

    .

--- wasm/solarsystem_bevy.js
+++
@@ -1,6 +0,0 @@
-import * as wasm from "./solarsystem_bevy_bg.wasm";
-import { __wbg_set_wasm } from "./solarsystem_bevy_bg.js";
-__wbg_set_wasm(wasm);
-export * from "./solarsystem_bevy_bg.js";
-
-wasm.__wbindgen_start();
